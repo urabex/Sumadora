8d82077c1271d4f24aae0487153fecd5aab7745f
uran556 <hiroki708.a48uk@gmail.com>
2021-05-29 19:22:31 +0000

    [fix]不要コード削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,5 @@
 module ApplicationHelper
   require "redcarpet"
-  require "coderay"
-
-  class HTMLwithCoderay < Redcarpet::Render::HTML
-    def block_code(code, language)
-      language = language.split(':')[0]
-
-      case language.to_s
-      when 'rb'
-        lang = 'ruby'
-      when 'yml'
-        lang = 'yaml'
-      when 'css'
-        lang = 'css'
-      when 'html'
-        lang = 'html'
-      when ''
-        lang = 'md'
-      else
-        lang = language
-      end
-
-      CodeRay.scan(code, lang).div
-    end
-  end
 
   def markdown(text)
     options = {
